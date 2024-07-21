706c3742e9c15ba8136cc7548efe402e7d51c4b5
Yashraj Chudasama <yashrajsinh.chudasama@tib.eu>
2024-07-21 18:46:08 +0200

    updating files

---
+++ KGEmbedding/link_prediction.py
@@ -0,0 +1,21 @@
+import argparse
+import pandas as pd
+from ranks import  tail_prediction, head_prediction
+import torch
+
+def main(args):
+    embedding_model = torch.load(args.results_path + args.model_name + '/trained_model.pkl')
+    pred = tail_prediction(embedding_model, args.head, args.relation, embedding_model.training)
+
+    return pred
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument('--results', type=str, required=True, help='path to stored results of trained kge model')
+    parser.add_argument('--model_name', type=str, required=True, help='name of kge model')
+    parser.add_argument('--head', type=str, required=True, help='head entity')
+    parser.add_argument('--relation', type=str, required=True, help='relation')
+
+    args = parser.parse_args()
+    main(args)
